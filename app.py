e008bfcd9da5a06a6cd7daa6f75158d9d429004e
beepscore <support@beepscore.com>
2017-10-09 12:46:19 -0700

    Add comments, add route cakes

--- app.py
+++ app.py
@@ -3,9 +3,17 @@ from flask import Flask
 
 app = Flask(__name__)
 
+# / is the website root, the entry point
+# http://127.0.0.1:5000
+# home http://127.0.0.1
+# port :5000
 @app.route('/')
 def index():
     return 'Hello world'
+
+@app.route('/cakes')
+def cakes():
+    return 'Yummy cakes!'
 
 if __name__ == '__main__':
     # '0.0.0.0' accessible to any device on the network
