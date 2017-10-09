8d0d68bd4bce804e4d8b5ef06c579c0f65c4ab52
beepscore <support@beepscore.com>
2017-10-09 13:10:32 -0700

    Add page.html, in app.py add route hello/<name>

--- app.py
+++ app.py
@@ -16,6 +16,12 @@ def index():
 def cakes():
     return 'Yummy cakes!'
 
+# route passes name from url to hello function
+@app.route('/hello/<name>')
+def hello(name):
+    # pass name from hello to render_template
+    return render_template('page.html', name=name)
+
 if __name__ == '__main__':
     # '0.0.0.0' accessible to any device on the network
     app.run(debug=True, host='0.0.0.0')
